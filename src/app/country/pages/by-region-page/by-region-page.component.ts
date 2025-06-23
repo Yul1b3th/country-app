dbb2e9b9ffb1c2b1a6b4bec9dad92e642779eac3
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-06-23 16:08:41 +0200

    Solución a la tarea

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
 
 @Component({
   selector: 'app-by-region',
