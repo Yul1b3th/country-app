00f87709e544211ace49e405704ae6636004facb
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-06-25 22:52:47 +0200

    Debounce - Búsquedas automáticas

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,4 +9,18 @@ import { Component, input, output } from '@angular/core';
 export class SearchInputComponent {
   placeholder = input<string>('Buscar...');
   value = output<string>();
+  debounceTime = input<number>(300);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
 import type { RESTCountry } from '../interfaces/rest-countries.interface';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
